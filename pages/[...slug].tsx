19cc1fec5a266f979fdff7e43ef85f3dcc22aaa5
peter_vida <csandragupta@gmail.com>
2024-01-25 23:51:23 +0100

    image exif data

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -2,12 +2,14 @@ import path from 'path';
 import fs from 'fs';
 import matter from 'gray-matter';
 import React from 'react';
-import { data } from 'autoprefixer';
+import ExifReader from 'exifreader';
 import Image from 'next/image';
 
 type pathsObject = {
   paths: Array<{ params: { slug: Array<string> } }>
 };
+
+// ---- PATHS ----
 
 const sourceFile = (fil: string) => path.join(__dirname, path.relative(__dirname, '../landmarks/source-handler'), fil);
 
@@ -29,54 +31,86 @@ export async function getStaticPaths() {
   };
 }
 
+// ---- PROPS ---- 
+
 export async function getStaticProps({ params }: { params: { slug: Array<string> } }) {
+  // base variables
   const fullSlug = params.slug.join('/');
   const dirPath = path.join(
     __dirname,
     path.relative(__dirname, '../landmarks/dataSource'),
     fullSlug
   );
+  const ownFolderContent = fs.readdirSync(dirPath);
 
-  const ownFolder = fs.readdirSync(dirPath);
-  console.log('ditr', params.slug.join('/'));
-
-  const ownImageFiles = ownFolder.filter((file) => {
+  // helpers
+  const getImagesFrom = (folder: Array<string>) => folder.filter((file) => {
     const ext = path.extname(file).toLowerCase();
     return ['.jpg', '.jpeg', '.png', '.gif'].includes(ext);
-   }).map(image => `/dataSource/${fullSlug}/${image}`);
+  });
 
-  const ownMdFile = ownFolder.filter(file => file.endsWith('.md'))[0];
+  const getImagesWithSlug = (img: string) => `/dataSource/${fullSlug}/${img}`
 
-  const ownFileContent = fs.readFileSync(path.join(
-    dirPath,
-    ownMdFile
-  ), 'utf-8');
+  const getImageExif = async (img: string) => {
+    const tags: any = await ExifReader.load(`${dirPath}/${img}`);
 
-  const { data: ownData, content: ownContent } = matter(ownFileContent);
+    return {
+      desc: tags['ImageDescription']?.value[0],
+      caption: tags['Caption/Abstract']?.value.map((byte: number) => String.fromCharCode(byte)).join(''),
+    };
+  };
+
+  const getMarkDownContent = (folderContent: Array<string>) => {
+    const ownMdFile = folderContent.filter(file => file.endsWith('.md'))[0];
+    const ownFileContent = fs.readFileSync(path.join(
+      dirPath,
+      ownMdFile
+    ), 'utf-8');
+
+    const { data, content } = matter(ownFileContent);
+    return { data, content };
+  }
+
+  const imageObjectBuilder = async (img: string) => {
+    const { desc, caption } = await getImageExif(img);
+
+    return {
+      url: getImagesWithSlug(img),
+      altText: { desc, caption },
+    }
+  }
+
+  // logic
+  const ownImageFiles = getImagesFrom(ownFolderContent);
+
+  const { data: ownData, content: ownContent } = getMarkDownContent(ownFolderContent)
   ownData.fullSlug = fullSlug;
+
+  const ownImageArray = await Promise.all(ownImageFiles.map( async (img) => await imageObjectBuilder(img)));
 
   return {
     props: {
       data: ownData,
       content: ownContent,
       images: {
-        own: ownImageFiles
+        own: ownImageArray,
       }
     },
   };
 }
 
 
-const PoiPage = ({ data, content, images }: {data: any, content: any, images: any}) => {
+const PoiPage = ({ data, content, images }: { data: any, content: any, images: any }) => {
   console.log('data:', images);
   // console.log('content:', content);
   return (
     <>
       <div>{data.name}{content}</div>
-      <Image src={images.own[0]} alt="alt" width={300} height={300}/>
+<Image src={images.own[0].url} alt={images.own[0].altText.desc} width={300} height={300} />
     </>
   )
 }
 
 export default PoiPage;
-
+/*
+*/
