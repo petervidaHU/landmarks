9043d728ea9090830e3f8bc97643930dd948e04f
peter_vida <csandragupta@gmail.com>
2024-01-25 18:22:25 +0100

    load images on slug.tsx

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -2,22 +2,26 @@ import path from 'path';
 import fs from 'fs';
 import matter from 'gray-matter';
 import React from 'react';
+import { data } from 'autoprefixer';
+import Image from 'next/image';
 
 type pathsObject = {
-  paths: Array<{ params: { slug: Array<string>} }>
-}
+  paths: Array<{ params: { slug: Array<string> } }>
+};
+
+const sourceFile = (fil: string) => path.join(__dirname, path.relative(__dirname, '../landmarks/source-handler'), fil);
 
 export async function getStaticPaths() {
   const slugFiles = {
-    cityFile: path.join(__dirname, path.relative(__dirname, '../landmarks/source-handler'), 'slugs-city.json'),
-    listFile: path.join(__dirname, path.relative(__dirname, '../landmarks/source-handler'), 'slugs-list.json'),
-    poiFile: path.join(__dirname, path.relative(__dirname, '../landmarks/source-handler'), 'slugs-poi.json'),
+    cityFile: sourceFile('slugs-city.json'),
+    listFile: sourceFile('slugs-list.json'),
+    poiFile: sourceFile('slugs-poi.json'),
   }
 
-  const slugsData = Object.values(slugFiles).reduce((acc: Array<pathsObject>, curr) => {
-    const t = [...acc, ...JSON.parse(fs.readFileSync(curr, 'utf-8'))]
-    return t;
-  }, [])
+  const slugsData = Object.values(slugFiles).reduce((acc: Array<pathsObject>, curr) => [
+    ...acc,
+    ...JSON.parse(fs.readFileSync(curr, 'utf-8'))
+  ], [])
 
   return {
     paths: slugsData,
@@ -25,27 +29,52 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }: {params: { slug: Array<string>}}) {
-  const dirPath = path.join(__dirname, path.relative(__dirname, '../landmarks/dataSource'), params.slug.join('/'));
-  const mdFile = fs.readdirSync(dirPath).filter(file => file.endsWith('.md'))[0];
-  const fileContent = fs.readFileSync( path.join(dirPath, mdFile), 'utf-8');
+export async function getStaticProps({ params }: { params: { slug: Array<string> } }) {
+  const fullSlug = params.slug.join('/');
+  const dirPath = path.join(
+    __dirname,
+    path.relative(__dirname, '../landmarks/dataSource'),
+    fullSlug
+  );
 
-  const { data, content } = matter(fileContent);
+  const ownFolder = fs.readdirSync(dirPath);
+  console.log('ditr', params.slug.join('/'));
+
+  const ownImageFiles = ownFolder.filter((file) => {
+    const ext = path.extname(file).toLowerCase();
+    return ['.jpg', '.jpeg', '.png', '.gif'].includes(ext);
+   }).map(image => `/dataSource/${fullSlug}/${image}`);
+
+  const ownMdFile = ownFolder.filter(file => file.endsWith('.md'))[0];
+
+  const ownFileContent = fs.readFileSync(path.join(
+    dirPath,
+    ownMdFile
+  ), 'utf-8');
+
+  const { data: ownData, content: ownContent } = matter(ownFileContent);
+  ownData.fullSlug = fullSlug;
 
   return {
     props: {
-      data,
-      content,
+      data: ownData,
+      content: ownContent,
+      images: {
+        own: ownImageFiles
+      }
     },
   };
 }
 
 
-const PoiPage = ({ data, content }) => {
-  console.log('data:', data);
-  console.log('content:', content);
+const PoiPage = ({ data, content, images }: {data: any, content: any, images: any}) => {
+  console.log('data:', images);
+  // console.log('content:', content);
   return (
-    <div>{data.name}{content}</div>
+    <>
+      <div>{data.name}{content}</div>
+      <Image src={images.own[0]} alt="alt" width={300} height={300}/>
+    </>
   )
 }
 
